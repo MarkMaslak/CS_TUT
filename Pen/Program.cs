9cd8fcea340bc297ce6a68a2e6d3e77812f5ca13
Refrimatik <info@refrimatik.com>
2016-10-19 21:07:58 +0200

    Separete class Pen into a separated file.

--- Pen/Program.cs
+++ Pen/Program.cs
@@ -92,37 +92,5 @@ namespace Pen
     }
 
 
-    public class Pen
-    {
-
-        /// <summary>
-        /// Event informs that cana changed. Event of <see cref="Pen"/> class.
-        /// </summary>
-        public event EventHandler<PenEventArgs> CanaCambiada;
-
-
-        protected virtual void OnCanaCambiada(int valorCana)
-        {
-            CanaCambiada.Raise(this, new PenEventArgs(valorCana));
-        }
-
-
-
-        private int _cana;
-        public int Cana
-        {
-            get { return _cana; }
-            set
-            {
-                if (value == _cana)
-                    return;
-
-                _cana = value;
-                OnCanaCambiada(_cana);
-            }
-        }
-
-
-    }
-
+ 
 }
